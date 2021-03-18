95c1465d34ca07641b63ccb77b380906336b4730
Glaucio Monteiro <glaucio.91@hotmail.com>
2021-03-18 09:25:01 -0300

    dxando bom =)

--- Linux/ubuntu18_04.sh
+++ Linux/ubuntu18_04.sh
@@ -3,7 +3,7 @@
 sudo apt-get -fy remove libreoffice-* && sudo apt-get -fy autoremove;
 #instalar o openVPN
 sudo apt-get install -fy openvpn network-manager-openvpn network-manager-openvpn-gnome;
-#instalar Wine (Para rodar aplicação .exe - Alguns!)
+#instalar Wine (Para rodar aplicação .exe - Algumas!)
 wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -;
 sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main';
 sudo apt-get install -fy libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386;
